6c695597dcf0297b8e8217d2a3590413199588c3
florallll <49378266+florallll@users.noreply.github.com>
2020-08-17 18:23:11 +0800

    Create exchange_configs.js

---
+++ configs/exchange_configs.js
@@ -0,0 +1,139 @@
+{
+  "Binance": {
+    "SIZE_DECIMAL": {
+      "BTCUSD|spot": 6,
+      "ETHUSD|spot": 2,
+      "EOSUSD|spot": 2,
+      "BCHUSD|spot": 2,
+      "ETHBTC|spot": 2,
+      "BCHBTC|spot": 2,
+      "EOSBTC|spot": 0,
+      "XRPBTC|spot": 0,
+      "LTCBTC|spot": 2,
+      "TRXUSD|spot": 1,
+      "ADAUSD|spot": 1,
+      "XRPUSD|spot": 1,
+      "XRPETH|spot": 0,
+      "ADABTC|spot": 0,
+      "TRXBTC|spot": 0,
+      "LINKBTC|spot": 0,
+      "TRXETH|spot": 0
+    }
+  },
+  "Coinbene": {
+    "SIZE_DECIMAL": {
+      "BTCUSDT|perp": 2,
+      "ETHUSDT|perp": 2,
+      "EOSUSDT|perp": 2,
+      "LTCUSDT|perp": 2
+    }
+  },
+  "Huobi": {
+    "SIZE_DECIMAL": {
+      "BTCUSD|spot": 2,
+      "ETHUSD|spot": 2
+    }
+  },
+  "OKcoin": {
+    "SIZE_DECIMAL": {
+      "BTCUSD|quarter": 0,
+      "ETHUSD|quarter": 0,
+      "EOSUSD|quarter": 0,
+      "XRPUSD|quarter": 0,
+      "LTCUSD|quarter": 0,
+      "BCHUSD|quarter": 0,
+      "BTCUSD|this_week": 0,
+      "ETHUSD|this_week": 0,
+      "EOSUSD|this_week": 0,
+      "XRPUSD|this_week": 0,
+      "LTCUSD|this_week": 0,
+      "BCHUSD|this_week": 0
+    }
+  },
+  "Bitflyer": {
+    "SIZE_DECIMAL": {
+      "BTCJPY|spot": 2,
+      "BTCJPY|lightening|fx": 2
+    }
+  },
+  "Upbit": {
+    "SIZE_DECIMAL": {
+      "BTCKRW|spot": 2,
+      "ETHKRW|spot": 2,
+      "EOSKRW|spot": 2
+    }
+  },
+  "Bithumb": {
+    "SIZE_DECIMAL": {
+      "BTCKRW|spot": 2,
+      "ETHKRW|spot": 2,
+      "EOSKRW|spot": 2
+    }
+  },
+  "Bitstamp": {
+    "SIZE_DECIMAL": {
+      "BTCUSD|spot": 2,
+      "ETHUSD|spot": 2,
+      "XRPUSD|spot": 2
+    }
+  },
+  "Quoinex": {
+    "SIZE_DECIMAL": {
+      "BTCJPY|spot": 2,
+      "ETHJPY|spot": 2,
+      "XRPJPY|spot": 2
+    }
+  },
+  "HuobiFuture": {
+    "SIZE_DECIMAL": {
+      "BTCUSD|quarter": 0,
+      "ETHUSD|quarter": 0,
+      "EOSUSD|quarter": 0,
+      "LTCUSD|quarter": 0,
+      "BCHUSD|quarter": 0,
+      "BTCUSD|this_week": 0,
+      "ETHUSD|this_week": 0,
+      "EOSUSD|this_week": 0,
+      "LTCUSD|this_week": 0,
+      "BCHUSD|this_week": 0
+    }
+  },
+  "Deribit": {
+    "SIZE_DECIMAL": {
+      "BTCUSD|perp": 4,
+      "ETHUSD|perp": 4,
+      "BTCUSD|option": 1,
+      "ETHUSD|option": 1
+    }
+  },
+  "Bybit": {
+    "SIZE_DECIMAL": {
+      "BTCUSD|perp": 0,
+      "ETHUSD|perp": 0
+    }
+  },
+  "Ftx": {
+    "SIZE_DECIMAL": {
+      "BTCUSD|perp": 2,
+      "ETHUSD|perp": 2,
+      "BTCUSD|spot": 2,
+      "ETHUSD|spot": 2
+    }
+  },
+  "Coinflex": {
+    "SIZE_DECIMAL": {
+      "BTCUSD|this_month": 2,
+      "BTCUSD|next_month": 2
+    }
+  },
+  "Fcoin": {
+    "SIZE_DECIMAL": {
+      "BTCUSD|spot": 2,
+      "ETHUSD|spot": 2,
+      "EOSUSD|spot": 2,
+      "LTCUSD|spot": 2,
+      "XRPUSD|spot": 2,
+      "BCHUSD|spot": 2
+    }
+  }
+}
